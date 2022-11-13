398390885e84b19690198c472821ea03a3eb2358
Loris-Beranger <lolo.beranger1@gmail.com>
2022-11-13 21:02:44 +0100

    page accueil

---
+++ components/Bottom.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './bottom.module.css'
+const Bottom = () => {
+  return (
+    <div className={styles.bottomContainer}>
+      <button className={styles.bottomButton}>Book now</button>
+    </div>
+  )
+}
+
+export default Bottom
